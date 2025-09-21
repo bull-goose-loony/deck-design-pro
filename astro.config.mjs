405c9291b6ebcb222daf0a0c76c5c6c47e6a9493
bull-goose-loony <zagrdev@gmail.com>
2025-09-21 14:24:07 -0600

    Setup bare-bones skeleton

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,35 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@tailwindcss/vite';
 
-import tailwindcss from '@tailwindcss/vite';
+// env helper
+import { loadEnv } from 'vite';
+
+// Storyblok
+import { storyblok } from '@storyblok/astro';
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
+
+const { STORYBLOK_DELIVERY_API_TOKEN } = loadEnv(
+	import.meta.env.MODE,
+	process.cwd(),
+	'',
+);
 
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwind()],
   },
-
-  integrations: [mdx()]
+  integrations: [
+    mdx(), 
+    sitemap(),
+    storyblok({
+      accessToken: env.STORYBLOCK_DELIVERY_API_TOKEN,
+      apiOptions: {
+        region: 'us',
+      },
+    }),
+  ],
+  output: 'server'
 });
